f0eaae4d75b0434225a131ad65d1a6c5ce6c5fc0
Lynt <strawww@gmail.com>
2017-05-20 20:25:53 +1000

    Test validate with stubs

--- test/validate_test.js
+++ test/validate_test.js
@@ -1,9 +1,17 @@
 const { expect } = require('chai');
+const { stub } = require('sinon');
 const { validate } = require('../scripts/validate');
 
 describe('#validate', () => {
   it('does not submit the form passed in if other is selected and there is no text', () => {
-    throw new Error('Implement this test');
+    const submitStub = stub();
+    const eventStub = stub();
+    const form = { submit: submitStub };
+    const event = { preventDefault: eventStub };
+    const validateForm = validate(form);
+    validateForm(event);
+    expect(eventStub.called).to.equal(true);
+    expect(submitStub.called).to.equal(false);
   });
 
   it('submits the form passed in if other is not selected', () => {
