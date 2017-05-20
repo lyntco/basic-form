30e3352345508b2c79e186c26eaee3cef75268cf
Lynt <strawww@gmail.com>
2017-05-20 20:16:05 +1000

    Add test cases

---
+++ test/validate_test.js
@@ -0,0 +1,12 @@
+const { expect } = require('chai');
+const { validate } = require('../scripts/validate');
+
+describe('#validate', () => {
+  it('does not submit the form passed in if other is selected and there is no text', () => {
+    throw new Error('Implement this test');
+  });
+
+  it('submits the form passed in if other is not selected', () => {
+    throw new Error('Implement this test');
+  });
+});
